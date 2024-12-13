61fc7deb6c0bcf9b0e99f1f498a711cc91c7b32a
skoppula22 <sruthi.koppula22@gmail.com>
2024-12-13 13:33:01 -0600

    added state file

---
+++ state/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "devank"
+
+
+
+versioning {
+    enabled = true
+}
+
+#lifecycle {
+#    prevent_destroy = true
+#    }
+}
+
